647e0a426c7f71026b015fee0c3d0fa8ea3d9443
aziz.koeksal <aziz.koeksal@58eeeec0-b036-0410-bbeb-a7c5fd3a3339>
2008-11-27 23:08:55 +0000

    Fixed the HTML Help Workshop installation script.

--- misc/install_hhw.sh
+++ misc/install_hhw.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+# Author: Aziz Köksal <aziz.koeksal@gmail.com>
+# History:
+#   27 Nov 2008: Fixes: the script didn't actually work, some commands were
+#                commented out.
+#                Determine the path of wine's system32/ using winepath.
+#   08 Sep 2008: First release.
+
 NL="
 "
 echo "Downloading htmlhelp.exe from download.microsoft.com:$NL"
-# wget 'http://download.microsoft.com/download/0/a/9/0a939ef6-e31c-430f-a3df-dfae7960d564/htmlhelp.exe' -O htmlhelp.exe
-echo -e "Need to get mfc40.dll, itircl.dll, itss.dll from a Windows folder or the"\\n"internet. "
+wget 'http://download.microsoft.com/download/0/a/9/0a939ef6-e31c-430f-a3df-dfae7960d564/htmlhelp.exe' -O htmlhelp.exe
+echo -e "Need to get mfc40.dll, itircl.dll, itss.dll from a Windows folder or the"\\n"internet."
 read -p "Download from the internet? [Y/n] " && [ -z "$REPLY" ] && REPLY="y"
 if [ "$REPLY" == "y" ]; then
-  wget 'http://www.dlldump.com/dllfiles/M/mfc40.dll' -O mfc40.dll \
-  --referer='http://www.dlldump.com/download-dll-files_new.php/dllfiles/M/mfc40.dll/4.1.001/download.html'
-  wget 'http://www.dlldump.com/dllfiles/I/itircl.dll' -O itircl.dll \
-  --referer='http://www.dlldump.com/download-dll-files_new.php/dllfiles/I/itircl.dll/5.2.3790.1159/download.html'
-  wget 'http://www.dlldump.com/dllfiles/I/itss.dll' -O itss.dll \
-  --referer='http://www.dlldump.com/download-dll-files_new.php/dllfiles/I/itss.dll/5.2.3790.1221/download.html'
+  DLLDUMP="http://www.dlldump.com/dllfiles"
+  REFERRER="http://www.dlldump.com/download-dll-files_new.php/dllfiles"
+  wget "$DLLDUMP/M/mfc40.dll" -O mfc40.dll --referer="$REFERRER/M/mfc40.dll/4.1.001/download.html"
+  wget "$DLLDUMP/I/itircl.dll" -O itircl.dll --referer="$REFERRER/I/itircl.dll/5.2.3790.1159/download.html"
+  wget "$DLLDUMP/I/itss.dll" -O itss.dll --referer="$REFERRER/I/itss.dll/5.2.3790.1221/download.html"
   #wget 'http://www.afreeocx.com/ocx/get/3340/hhctrl.ocx' -O hhctrl.ocx
 else
   read -p "Enter the path to your Windows installation (e.g. /mount/winxp/):$NL"
   echo ""
-  #WINDOWS=`ls -l $REPLY | grep windows -io`
   [ ! -d $REPLY ] && echo "The directory '$REPLY' doesn't exist." && exit;
   WINDOWS=`find $REPLY -maxdepth 1 -iname windows -type d`
   [ ! -d $WINDOWS ] && echo "Couldn't find the Windows directory in '$REPLY'." && exit;
@@ -26,19 +31,18 @@ else
   function fetchfile {
     DLL=`find $SYSTEM32 -maxdepth 1 -iname $1 -type f`
     [ ! -f $DLL ] && echo "Couldn't find $1 in '$SYSTEM32'." && exit;
+    cp $DLL ./$1
     echo "Found and fetched $DLL"
-    cp $DLL $1
   }
   fetchfile mfc40.dll; fetchfile itss.dll; fetchfile itircl.dll;
 fi
 
 echo "${NL}Starting setup."
-# wine htmlhelp.exe
+wine htmlhelp.exe
 
 echo "${NL}Copying mfc40.dll, itircl.dll and itss.dll to C:\\Windows\\system32.$NL"
-cp mfc40.dll itircl.dll itss.dll ~/.wine/drive_c/windows/system32/
-# Doesn't work. Wants to overwrite itircl.dll with mfc40.dll. Bug in cmd.exe's copy?
-# wine cmd.exe "/c copy mfc40.dll itircl.dll itss.dll 'C:\Windows\system32\'"
+cp mfc40.dll itircl.dll itss.dll `winepath 'C:\windows\system32\'`
+
 echo "${NL}Registering native DLL files.$NL"
 wine regsvr32.exe 'C:\Windows\system32\mfc40.dll'\
                   'C:\Windows\system32\itss.dll' \
